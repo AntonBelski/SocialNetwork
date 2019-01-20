1fd701f55c44a6768c07037f611c62281e44dbae
Anton Belski <anthony.belski@gmail.com>
2019-01-20 16:22:26 +0300

    Add the fields Register, Login and Logout.

---
+++ SocialNetwork/Components/MenuPartsViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace SocialNetwork.Components
+{
+    public class MenuPartsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (HttpContext.User.Identity.Name == null)
+            {
+                var login = Url.Action("Login", "Account");
+                var html = @"<li><a href=""{0}"" class=""navbar-brand"">Login</a></li>";
+                return new HtmlContentViewComponentResult(
+                    new HtmlString(string.Format(html, login)));
+            }
+            else
+            {
+                var logout = Url.Action("Logout", "Account");
+                var html = @"<li><a href=""{0}"" class=""navbar-brand"">Logout</a></li>";
+                return new HtmlContentViewComponentResult(
+                    new HtmlString(string.Format(html, logout)));
+            }
+        }
+    }
+}
